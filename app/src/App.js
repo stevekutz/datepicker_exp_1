91510f94e69119d05ceff40e73c73d943dc87c03
stevekutz <stkutz@gmail.com>
2019-09-19 23:15:13 -0500

    setup basic datepicker

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'react-rainbow-components';
 
-function App() {
-  return (
-    <div className="App">
-      <something />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    date: new Date(),
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="rainbow-p-vertical_large rainbow-p-horizontal_xx-large rainbow-m-horizontal_xx-large">
+            <DatePicker
+                value={this.state.date}
+                label="DatePicker Label"
+                onChange={ value => this.setState({ date: value }) } 
+            />
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
