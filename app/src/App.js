a38d03a93b624f42b039d3037c28ab2dcfe0a3c4
stevekutz <stkutz@gmail.com>
2019-09-20 21:30:49 -0500

    experimenting formatting of DatePicker

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { DatePicker } from 'react-rainbow-components';
+import { DatePicker , Card } from 'react-rainbow-components';
 
 class App extends React.Component {
   state = {
     date: new Date(),
+    date_1: new Date('October 31, 2010 03:24:00'),
   }
 
   render(){
     return (
-      <div className="App">
-        <div className="rainbow-p-vertical_large rainbow-p-horizontal_xx-large rainbow-m-horizontal_xx-large">
+  
+        <Card style = {{width: `50%`}} center>
             <DatePicker
                 value={this.state.date}
                 label="DatePicker Label"
                 onChange={ value => this.setState({ date: value }) } 
             />
-        </div>
-      </div>
+            <DatePicker
+                value={this.state.date_1}
+                label="DatePicker Label"
+                onChange={ value => this.setState({ date_1: value }) } 
+            />
+            <DatePicker
+            value={this.state.date}
+            label="DatePicker Label"
+            onChange={ value => this.setState({ date: value }) } 
+            />
+        </Card>
+
     );
 
   }
